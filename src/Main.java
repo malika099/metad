3c259ee558aa74050a11cfa79a10cdcf8f01e664
malika099 <ergasevamadina77@gmail.com>
2024-11-20 16:13:04 -0800

    get sum

---
+++ src/Main.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       /* Scanner sc = new Scanner(System.in);
+        int[]array = new int[1];
+        getSum(array);
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 1) {
+            hello();
+        } else if (a == 0) {
+            bye();
+        }
+    }
+    public static void hello() {
+        System.out.println("салам");
+    }
+    public static void bye() {
+        System.out.println("корушкончо");*/
+        //
+   /* public static void aa(){
+        for (int i = 2; i <= 10; i++) {
+            System.out.println();
+            for (int j = 0; j < 10; j++) {
+
+            System.out.println(i+"*"+i+"="+i*i);
+
+    public static void getSum (int[]a){
+        System.out.println(a);*/
+
+        /*Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum= (a , );
+
+
+    }
+    public static void sum(int a, int b){
+
+        int c = a+b;
+        int c1 = c/2;
+        System.out.println(c1);
+        
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        sum(a , b);
+    }
+    public static void sum(int a, int b) {
+        int c = a+b;
+        int d = c/2;
+        System.out.println(d);*/
+
+
+
+       /* Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sum(a);
+    }
+    public static void sum (int count){
+        for (int i = 0; i < count; i++) {
+            System.out.print("*");*/
+
+
+
+
+
+        }
+        public static void mm(){
+
+        }
+    }
