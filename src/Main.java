1fbbe69e76131e2253623d6ae6f16841f21f4534
ergasevamadina77@gmail.com <ergasevamadina77@gmail.com>
2025-01-15 13:05:45 -0800

    zxcvbn

--- src/Main.java
+++ src/Main.java
@@ -63,56 +63,7 @@ public class Main {
     public static void sum (int count){
         for (int i = 0; i < count; i++) {
             System.out.print("*");*/
-        System.out.println("dc");
-        System.out.println("asc");
-        System.out.println("dslcmwp");
-        System.out.println("dscwd");
-        System.out.println("dsc");
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
-        System.out.println(6);
-        System.out.println(7);
-        System.out.println(8);
-        System.out.println(9);
-        System.out.println(10);
-        System.out.println(11);
-        System.out.println("are you jn holiday");
-        System.out.println(12);
-        System.out.println(13);
-        System.out.println(14);
-        System.out.println(15);
-        System.out.println(16);
-        System.out.println(17);
-        System.out.println(18);
-        System.out.println(19);
-        System.out.println(20);
-        System.out.println(21);
-        System.out.println(22);
-        System.out.println(23);
-        System.out.println(24);
-        System.out.println(25);
-        System.out.println(26);
-        System.out.println(27);
-        System.out.println(28);
-        System.out.println(29);
-        System.out.println(30);
-        System.out.println(31);
-        System.out.println(32);
-        System.out.println(33);
-        System.out.println(34);
-        System.out.println(35);
-        System.out.println(36);
-        System.out.println(37);
-        System.out.println(38);
-        System.out.println(39);
-        System.out.println(40);
-        System.out.println(41);
-        System.out.println(42);
-        System.out.println(43);
-        System.out.println(44);
+
 
 
         }
